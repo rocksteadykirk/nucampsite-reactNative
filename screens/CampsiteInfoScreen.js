bae9afaa9a9d4cf37fdeaab0c6bab6b7741de728
rocksteadykirk <120696023+rocksteadykirk@users.noreply.github.com>
2023-11-29 20:41:56 +0900

    added flatlist component to render and display comments  for each campsite in the CampsiteInfo screen

--- screens/CampsiteInfoScreen.js
+++ screens/CampsiteInfoScreen.js
@@ -1,16 +1,144 @@
+import { useState } from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 import RenderCampsite from '../features/campsites/RenderCampsite';
+import { COMMENTS } from '../shared/comments';
 
-const CampsiteInfoScreen = ({route}) => {
-    const { campsite } = route.params;                    
-    return <RenderCampsite campsite={campsite} />;
+const CampsiteInfoScreen = ({ route }) => {
+    const { campsite } = route.params;
+
+    const [comments, setComments] = useState(COMMENTS);
+    const [favorite, setFavorite] = useState(false);
+
+    const renderCommentItem = ({ item }) => {
+        return (
+            <View style={styles.commentItem}>
+                <Text style={{ fontSize: 14 }}>{item.text}</Text>
+                <Text style={{ fontSize: 12 }}>{item.rating} Stars</Text>
+                <Text style={{ fontSize: 12 }}>
+                    {`-- ${item.author}, ${item.date}`}
+                </Text>
+            </View>
+        );
+    };
+
+    return (
+        <FlatList
+            data={comments.filter(
+                (comment) => comment.campsiteId === campsite.id
+            )}
+            renderItem={renderCommentItem}
+            keyExtractor={(item) => item.id.toString()}
+            contentContainerStyle={{
+                marginHorizontal: 20,
+                paddingVertical: 20
+            }}
+            ListHeaderComponent={
+                <>
+                    <RenderCampsite
+                        campsite={campsite}
+                        isFavorite={favorite}
+                        markFavorite={() => setFavorite(true)}
+                    />
+                    <Text style={styles.commentsTitle}>Comments</Text>
+                </>
+            }
+        />
+    );
 };
+
+const styles = StyleSheet.create({
+    commentsTitle: {
+        textAlign: 'center',
+        backgroundColor: '#fff',
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: '#43484D',
+        padding: 10,
+        paddingTop: 30
+    },
+    commentItem: {
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        backgroundColor: '#fff'
+    }
+});
 
 export default CampsiteInfoScreen;
 
+/*
+
+In the createStackNavigator configuration, the options property takes a function that receives a route object. It extracts the campsite parameter from the route to dynamically set the title of the screen based on the campsite's name.
+
+In the functional component CampsiteInfoScreen, the route is received as a prop. It contains information about the navigation route, including parameters passed to it. This allows access to the campsite parameter, facilitating dynamic rendering based on the selected campsite.
+
+The route object is destructured to extract the campsite parameter, which holds information about the selected campsite. This information is used to display details about the specific campsite within the CampsiteInfoScreen component.
+
+In the renderCommentItem function, the parameter { item } refers to an element of the comments array. This structure is common in React Native's FlatList component, where each item in the data array is passed as a parameter to the renderItem function for rendering in the list. In this case, it renders a view containing comment details such as text, rating, author, and date.
+
+User
+In less than 100 words each
+
+1. what is the renderItem function and provide the syntax / structure?
+2. is renderCommentItem a renderItem function?
+3. How is {item} connected to the comments array?
+
+ChatGPT
+renderItem Function:
+The renderItem function is used in React Native's FlatList component to specify how each item in the data array should be rendered. Its structure typically includes a parameter (commonly named { item }) representing the current item being processed, and it returns the JSX (UI elements) to be rendered for that item.
+
+const renderItem = ({ item }) => {
+    // JSX for rendering the item
+};
+renderCommentItem as renderItem:
+Yes, renderCommentItem serves as a custom renderItem function. It defines how individual comment items should be rendered within the FlatList.
+
+{item} and Comments Array:
+{item} is the parameter used within the renderCommentItem function to represent an element of the comments array. In the FlatList component, the data prop is set to the filtered comments array, and for each item in this array, the renderItem function (renderCommentItem in this case) is called to render the corresponding UI elements. The connection is established through the data passed to the FlatList.
 
 
-// using RenderCampsite to pass it a prop to tell it what to render.
-// to get the campsite param from the route.params object which is where all the params for this rout e are located
 
 
+In less than 100 words each:
+how is a renderItem function declared?
+ChatGPT
+A renderItem function is declared as a JavaScript arrow function, commonly taking a single parameter { item } to represent the current item in the data array. The function returns JSX code defining the UI elements for rendering that specific item. This function is then passed as a prop to the FlatList component in React Native, specifying how each item in the list should be rendered. The structure typically looks like:
+
+javascript
+Copy code
+const renderItem = ({ item }) => {
+    // JSX for rendering the item
+};
+User
+in less than 150 words
+do we have to name the renderItem function renderItem? How is the renderItem function declared ?
+How does react know that it is a renderItem function? 
+ChatGPT
+No, the renderItem function doesn't have to be named renderItem; it's a convention. When defining a custom renderItem function, you can choose any valid function name. The essential factor is that you pass this function as the renderItem prop to the FlatList component. React doesn't inherently recognize it as "renderItem"; rather, it interprets the function passed to the renderItem prop and utilizes it during the rendering process. The declaration follows the typical structure of a JavaScript arrow function, often taking a parameter like { item } to represent the current item in the data array and returning JSX code for rendering. The association between your custom function and the rendering process is established by explicitly passing it as the renderItem prop when configuring the FlatList.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+The FlatList needs to be at the top of the components tree to properly calculate scroll height nd to make the entire page scrollable
+using RenderCampsite to pass it a prop to tell it what to render.
+to get the campsite param from the route.params object which is where all the 
+params for this rout e are located
+
+*/
 
